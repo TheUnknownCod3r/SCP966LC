15fb1a191992fc3106df83748c30e1f171e6d9cc
FelixAllard <xilef992@gmail.com>
2024-06-01 11:46:16 -0400

    Building

---
+++ obj/Release/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SCP966
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "TheWeavers.SCP966";
+        public const string PLUGIN_NAME = "SCP966";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ src/Ai/SCP966.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using GameNetcodeStuff;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace SCP966.Ai;
+public class Scp966 : EnemyAI
+{
+    public SkinnedMeshRenderer mesh;
+    public Transform lookAt;
+    public Transform defaultLookAt;
+
+
+    [NonSerialized] public bool isVisibleForLocalPlayer;
+    private PlayerControllerB localPlayer;
+
+    public override void Start()
+    {
+        base.Start();
+        localPlayer = RoundManager.Instance.playersManager.localPlayerController;
+    }
+
+    public override void DoAIInterval()
+    {
+        base.DoAIInterval();
+        CheckIfLocalPlayerHasNightVisionClientRpc();
+        
+    }
+
+    /// <summary>
+    /// Check if night vision is enabled on local client
+    /// I DON'T KNOW IF THIS WORKS
+    /// </summary>
+    [ClientRpc]
+    private void CheckIfLocalPlayerHasNightVisionClientRpc()
+    {
+        if (localPlayer.nightVision.enabled == true )
+        {
+            isVisibleForLocalPlayer = true;
+            mesh.enabled = true;
+        }
+        else
+        {
+            isVisibleForLocalPlayer = false;
+            mesh.enabled = false;
+        }
+    }
+
+
+}
